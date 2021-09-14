760a7a8b543e6585ae17436925c60896672609ac
DESKTOP-OFS5H6F\User <frsummit1@gmail.com>
2021-09-14 22:50:06 +0600

    all warning removed

--- src/views/common/Footer.js
+++ src/views/common/Footer.js
@@ -6,7 +6,13 @@ class Footer extends Component {
         return (
             <div id="footer" className="footer-section bg-danger d-flex align-items-center">
                 <div className="footer-inner text-center w-100 d-flex align-items-center">
-                    <marquee>
+                    {/* <marquee>
+                        <div className="d-flex text-white">
+                            <p className="text-uppercase pr-2">This is JERP development site, </p>
+                            <p>Follow the instructions to embed the icon font in your site and learn how to style your icons using CSS.</p>
+                        </div>
+                    </marquee> */}
+                    <marquee behavior="scroll" direction="left">
                         <div className="d-flex text-white">
                             <p className="text-uppercase pr-2">This is JERP development site, </p>
                             <p>Follow the instructions to embed the icon font in your site and learn how to style your icons using CSS.</p>
--- src/views/common/TitleBreadcrumb.js
+++ src/views/common/TitleBreadcrumb.js
@@ -5,17 +5,38 @@ import { NavLink } from "react-router-dom"
 const TitleBreadcrumb = (props) => {
     const [title, setTitle] = useState('');
 
-    useEffect( () => {
+    useEffect(() => {
+        const initital = () => {
+            setTitle(props.titleBreadcrumb)
+        }
         console.log('I am here')
         console.log(props)
         console.log(props.titleBreadcrumb)
-        setTitle(props.titleBreadcrumb)
+        initital()
     })
+
+    /*useEffect(() => {
+        const fetchBusinesses = () => {
+            return fetch("theURL", { method: "GET" }
+            )
+                .then(res => normalizeResponseErrors(res))
+                .then(res => {
+                    return res.json();
+                })
+                .then(rcvdBusinesses => {
+                    // some stuff
+                })
+                .catch(err => {
+                    // some error handling
+                });
+        };
+        fetchBusinesses();
+    }, []);*/
 
     return (
         <div className="layout-breadcrumb">
             <div className="breadcrumb_area">
-                <h5>{ title }
+                <h5>{title}
                     {/* {
                         title === 'Dashboard' ?
                         <span> {props.titleBreadcrumb}</span> :
@@ -26,7 +47,7 @@ const TitleBreadcrumb = (props) => {
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item"><NavLink to="/features/users/dashboard">Features</NavLink></li>
                         <li className="breadcrumb-item"><NavLink to="/features/users/dashboard">Local Sales</NavLink></li>
-                        <li className="breadcrumb-item active" aria-current="page">{ title }</li>
+                        <li className="breadcrumb-item active" aria-current="page">{title}</li>
                     </ol>
                 </nav>
             </div>
