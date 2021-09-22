a5fed4978095046988b89c8ce30f223aae750a34
DESKTOP-OFS5H6F\User <frsummit1@gmail.com>
2021-09-22 16:17:59 +0600

    Redux description added

--- src/views/dashboard/CounterRedux.js
+++ src/views/dashboard/CounterRedux.js
@@ -65,3 +65,102 @@ const CounterRedux = () => {
     // }
 }
 export default CounterRedux;
+
+// REDUX
+/*
+* 1. Configure index.js as bellow
+        import { createStore } from 'redux';
+        import allReducer from './reducer'
+        import { Provider } from 'react-redux'
+
+        const store = createStore(
+            allReducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+        ReactDOM.render(
+            <Provider store={store}>
+                <App />
+            </Provider>,
+            document.getElementById('root')
+        );
+
+* 2. Create action 
+        export const increment = (nr) => {
+            return {
+                type: 'INCREMENT',
+                payload: nr
+            }
+        }
+        export const decrement = () => {
+            return {
+                type: 'DECREMENT'
+            }
+        }
+
+
+* 3. Create reducer
+        index -->
+        import counterReducer from "./counter";
+        import loggedReducer from "./isLogged";
+
+        import {combineReducers} from 'redux'
+
+        const allReducer = combineReducers({
+            counter: counterReducer,
+            isLogged: loggedReducer,
+        });
+        export default allReducer;
+
+        reducer/counter -->
+        const counterReducer = (state = 0, action) => {
+            switch (action.type) {
+                case 'INCREMENT':
+                    // return state + 1
+                    return state + action.payload
+                case 'DECREMENT':
+                    return state - 1
+                default:
+                    return state
+            }
+        };
+        export default counterReducer;
+
+        reducer/isLogged -->
+        const loggedReducer = (state = false, action) => {
+            switch (action.type) {
+                case 'SIGN_IN':
+                    return !state
+                default:
+                    return state
+            }
+        }
+        export default loggedReducer
+
+
+* 4. dispatch action from Component
+
+        import { useSelector, useDispatch } from 'react-redux'
+        import { increment, decrement } from '../../actions';
+
+        const CounterRedux = () => {
+
+            const counter = useSelector(state => state.counter)
+            const isLogged = useSelector(state => state.isLogged)
+            const dispatch = useDispatch()
+
+            return (
+                <div className="App">
+                    <h1>Counter {counter}</h1>
+                    <button onClick={() => dispatch(increment(5))}>+</button>
+                    <button onClick={() => dispatch(decrement())}>-</button>
+                    {
+                        isLogged ?
+                            <p>True</p> : 
+                            <p>False</p>
+                    }
+                </div>
+            );
+        }
+        export default CounterRedux;
+
+*/
